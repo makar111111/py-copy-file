f9c161fa09bf9715f7de8f06121a04b847fdbf74
Ruslan <makar80008@gmail.com>
2026-07-20 12:44:17 +0300

    Complite 1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    cp_command, source_file, destination_file = command.split()
+
+    if source_file == destination_file:
+        return
+
+    with open(source_file, "r") as file_in, \
+            open(destination_file, "w") as file_out:
+        file_out.write(file_in.read())
