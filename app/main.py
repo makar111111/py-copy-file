490f2514a4bbf8b706b7e8cada9dedfdafd355bc
Ruslan <makar80008@gmail.com>
2026-07-20 12:48:44 +0300

    Complite 2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
+import os
+
+
 def copy_file(command: str) -> None:
-    cp_command, source_file, destination_file = command.split()
+    command_parts = command.split()
+
+    if len(command_parts) != 3:
+        return
+
+    cp_command, source_file, destination_file = command_parts
+
+    if cp_command != "cp":
+        return
 
     if source_file == destination_file:
+        return
+
+    if not os.path.exists(source_file):
         return
 
     with open(source_file, "r") as file_in, \
